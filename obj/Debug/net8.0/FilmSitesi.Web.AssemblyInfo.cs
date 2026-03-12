22e2b42a6b6a8c8d6c378d2394f536538d142706
ISA FURKAN KACMAZ <isa.kacmaz@isuzu.com.tr>
2026-03-12 14:06:07 +0300

    Rating özelliğini 10 yıldız ile yapma ve daha hoş bir tasarım sunma

--- obj/Debug/net8.0/FilmSitesi.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/FilmSitesi.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa94497cbbd6062d5dc9dd56fae45f41d274b77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705224db35fda42aa941039f9fd547c4efaf552d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
