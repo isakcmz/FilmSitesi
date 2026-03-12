997d58d73d62bae0fa90da8ab4e5abbd85767b36
ISA FURKAN KACMAZ <isa.kacmaz@isuzu.com.tr>
2026-03-12 09:07:56 +0300

    İzlediklerim, izleme listem, yorumlarım ve bilgilerin bulunduğu Profil sayfası ekleme

---
+++ Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,18 @@
+using FilmSitesi.Web.Models.Entities;
+
+namespace FilmSitesi.Web.Models.ViewModels;
+
+public class ProfileViewModel
+{
+    public string UserName { get; set; } = string.Empty;
+
+    public int WatchedCount { get; set; }
+
+    public int WatchlistCount { get; set; }
+
+    public int ReviewCount { get; set; }
+
+    public List<WatchedMovie> RecentWatched { get; set; } = new();
+
+    public List<Review> RecentReviews { get; set; } = new();
+}
--- obj/Debug/net8.0/FilmSitesi.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/FilmSitesi.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e70276bb5bc9b89c066525220a5831ba2f82e004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f739e83303eb95fb250cbc5db1f71ed16045ff18")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
