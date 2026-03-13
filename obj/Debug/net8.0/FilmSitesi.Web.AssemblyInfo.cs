232f80cfb6031dfa4b2fe346c86802a2ae0e8504
ISA FURKAN KACMAZ <isa.kacmaz@isuzu.com.tr>
2026-03-13 10:06:16 +0300

    Profil sayfasına hover ekleme ve güncelleştirme

--- obj/Debug/net8.0/FilmSitesi.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/FilmSitesi.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092de7794e426b28718a25bb789cfb441cf6bf27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1277b809a458098514426b510f579a4b5637fd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
