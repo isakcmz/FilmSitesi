fc4af60360dd48dee101571452086aaea30eb06d
ISA FURKAN KACMAZ <isa.kacmaz@isuzu.com.tr>
2026-03-23 16:14:16 +0300

    Yorumları kimlerin beğendiğini gösterme ve anasayfadaki yorum beğenildi feed'ini güncelleme

--- obj/Debug/net8.0/FilmSitesi.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/FilmSitesi.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e067dfd764486ce7bed0342b032ff05f85ab1cee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b42496696951549d86eae0a390e17eb9b7abbfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmSitesi.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
